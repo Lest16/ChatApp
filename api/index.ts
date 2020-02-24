0772739b79d75274ba7fbab5668daaae0a3f26dc
Lest16 <piligrim16@mail.ru>
2020-02-24 22:49:18 +0300

    fixed api tsconfig

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,11 @@
-// подключение express
+
 import express from "express";
-// создаем объект приложения
+
 const app = express();
-// определяем обработчик для маршрута "/"
+
 app.get("/", (request, response) => {
 
-    // отправляем ответ
     response.send("<h2>Привет Express!</h2>");
 });
-// начинаем прослушивать подключения на 3000 порту
+
 app.listen(3000);
